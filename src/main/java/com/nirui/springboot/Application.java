1d90ca83809ef4ddb27b41582e35e276e6ef7b17
NIRUI <1026224633@qq.com>
2019-12-10 19:51:24 +0800

    pom修改

--- src/main/java/com/nirui/springboot/Application.java
+++ src/main/java/com/nirui/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.nirui.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement//开启事务管理
 //SpringBootServletInitializer打war包
+@MapperScan("com.nirui.springboot.mapper")
 public class Application extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
